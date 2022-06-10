b4037b702daa6107f5e56b8745814780e2673a22
Nicolas Minet <minet.nicolas@gmail.com>
2022-06-10 17:18:35 +0200

    adding kube module

--- lab.tf
+++ lab.tf
@@ -41,3 +41,19 @@ module "Bastion" {
   public_subnets_config = module.Networking.aws-public-subnets
   public_security_group = module.Security.public_security_group_id
 }
+
+module "Kube-cluster" {
+  source                = "./module/kube-cluster"
+  depends_on            = [
+    module.Networking,
+    module.Security
+  ]
+  region                = var.region
+  environment           = var.environment
+  vpc_cidr              = var.vpc_cidr
+  private_subnets_cidr  = var.private_subnets_cidr
+  availability_zones    = local.lab_availability_zones
+  ami_name              = local.lab_ami_name
+  private_subnets_config = module.Networking.aws-private-subnets
+  private_security_group = module.Security.private_security_group_id
+}
