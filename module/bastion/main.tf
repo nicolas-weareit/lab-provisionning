2b3e2426d2d687ae94c77656a292ed477fe4b69a
Nicolas Minet <nicolas.minet@we-are-it.fr>
2022-08-31 13:33:22 +0200

    adding prerequiste software for kube hard way

--- module/bastion/main.tf
+++ module/bastion/main.tf
@@ -22,7 +22,11 @@ resource "aws_instance" "devops_bastion" {
 sudo apt update
 sudo apt upgrade -y
 sudo hostnamectl set-hostname bastion${count.index}.${var.domain_name}
-
+wget -q --show-progress --https-only --timestamping \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssl \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssljson
+chmod +x cfssl cfssljson
+sudo mv cfssl cfssljson /usr/local/bin/
 
 EOF
   tags = {
